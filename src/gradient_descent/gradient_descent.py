964b49c097383d706e264be5b92c958adf002abf
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2026-01-31 17:33:28 +0100

    refactor: simplify gradient_descent script and externalize config

--- src/gradient_descent/gradient_descent.py
+++ src/gradient_descent/gradient_descent.py
@@ -1,9 +1,9 @@
 # import all the libraries needed
 import torch.nn as nn
 from torch.optim import SGD
+from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
 
 import sys
 from pathlib import Path
@@ -11,148 +11,127 @@ from pathlib import Path
 parent_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(parent_dir))
 
-from data_manager import load_model, save_model, MODEL_WEIGHTS_PATH, TRAIN_DATASET, TEST_DATASET
+import config
+import data_manager as data
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
-        # inherite everything from nn.Module
         super().__init__()
-        # define the neural net
-        # first layer has 784 neurons and passes the values onto 100 neurons in the second layer
-        self.Matrix1 = nn.Linear(28**2, 100)
-        # second layer has 100 neurons
-        # it processes data from the input layer and then sends the values to 50 new neurons
+
+        self.Matrix1 = nn.Linear(data.TENSOR, 100)
         self.Matrix2 = nn.Linear(100, 50)
-        # same as second layer but it has 50 neurons, passes it to 10 output neurons
-        # there are 10 output neurons because we have 10 different digits it can be
-        self.Matrix3 = nn.Linear(50, 10)
-        # define the rectified linear unit (ReLU)
+        self.Matrix3 = nn.Linear(50, data.NUM_CLASSES)
+
         self.rectifier = nn.ReLU()
 
     def forward(self, input_images):
-        # convert the tensor into an array as mentioned previously
-        input_images = input_images.view(-1, 28**2)
-        # pass the images through the layers
-        # use ReLU to make sure that only meaningful output gets passed to next neuron
+        input_images = input_images.view(-1, data.TENSOR_SIZE)
+
         input_images = self.rectifier(self.Matrix1(input_images))
         input_images = self.rectifier(self.Matrix2(input_images))
         input_images = self.Matrix3(input_images)
-        # return the predictions of the Neural Network
-        # use squeeze to remove unnecessary dimensions from the tensor
+
         return input_images.squeeze()
 
 def train_model(data_loader, neural_network, num_epochs):
-    print("Training model...")
-    # optimize the model using SGD
     optimizer = SGD(neural_network.parameters(), lr=0.01)
-    # define the loss function
     loss = nn.CrossEntropyLoss()
 
-    # train the model
-    # define the lists to store the data in
     losses = []
     epochs = []
-    # iterate through each epoch
+
     for epoch in range(num_epochs):
-        print(f'Training: Epoch {epoch+1}/{num_epochs}') # +1 to not start at the normal index of 0
-        for i, (images, labels) in enumerate(train_dataloader):
-            # update the weights of the neural network
-            # reset the stored gradients of the model to avoid errors
+        print(f'Training: Epoch {epoch + 1}/{num_epochs}')
+        for i, (images, labels) in enumerate(data_loader):
             optimizer.zero_grad()
-            # calculate the loss
+            
             loss_value = loss(neural_network(images), labels)
-            # use backpropagation to find the gradients of the weights
             loss_value.backward()
-            # adjust the weights in the direction of least loss
             optimizer.step()
-            # store the training data
-            epochs.append(epoch+1/len(train_dataloader))
+
+            epochs.append(epoch + 1 / len(data_loader))
             losses.append(loss_value.item())
-    # return the epochs and losses, converted to numpy arrays to better work with them
+
     return np.array(epochs), np.array(losses)
 
 def test_model(neural_network):
-    print("Testing model...")
-    # splice of the first 2000 entries in the test dataset
-    test_images, test_labels = TEST_DATASET[0:2000]
-    # get the predicted class of the neural network
+    test_images, test_labels = data.TEST_DATASET[0:40]
     test_label_predictions = neural_network(test_images).argmax(axis=1)
 
-    # define the figure and 2D array of subplots
     figure, axis = plt.subplots(4, 10, figsize=(22.5, 15))
 
     for i in range(40):
-        # navigate to a specific subplot
-        plt.subplot(4, 10, i+1)
-        # show the image for that subplot
+        plt.subplot(4, 10, i + 1)
         plt.imshow(test_images[i])
-        # set the title of the cell as the predicted image
         plt.title(f'Predicted Digit: {test_label_predictions[i]}')
-    # use the inbuilt tight_layout() function to properly space all cells and text
+    
     figure.tight_layout()
-    # show the plot in a new window
     plt.show()
 
 def average_epoch_and_loss_data(epoch_data, loss_data):
-    # calculate the average loss for each epoch so we can better visualize the Dataset
-    # split the loss and epoch data into num_epochs different batches
-    epoch_data_average = epoch_data.reshape(num_epochs, -1)
-    loss_data_average = loss_data.reshape(num_epochs, -1)
-    # calculate the mean loss of each epoch where axis 1 corresponds to the epochs
+    epoch_data_average = epoch_data.reshape(config.NUM_EPOCHS, -1)
+    loss_data_average = loss_data.reshape(config.NUM_EPOCHS, -1)
+    
     epoch_data_average = epoch_data_average.mean(axis=1)
     loss_data_average = loss_data_average.mean(axis=1)
-    # return the averaged epoch and loss data
+    
     return epoch_data_average, loss_data_average
 
 def plot_data(data_x, data_y):
-    print(f"Loss Data: {data_y}")
     plt.figure(1)
-    # plot the data as a graph of markers connected by dashed lines
+
     plt.plot(data_x, data_y, 'o--', color='blue', label='average cross entropy loss per epoch')
-    # set the labels and title of the graph
+    
     plt.xlabel('Epoch Number')
     plt.ylabel('Cross Entropy (averaged per epoch)')
     plt.title('Cross Entropy (averaged per epoch)')
+    
     plt.legend()
+    plt.show()
 
-def train_model_from_scratch():
-    # train the model and return the epoch data and loss data
-    epoch_data, loss_data = train_model(train_dataloader, neural_network, num_epochs)
-    # calculate the average of the epoch and loss for better visualization
+def run_training_mode(train_dataloader):
+    neural_network = NeuralNetwork()
+
+    print("Training model...")
+    epoch_data, loss_data = train_model(train_dataloader, neural_network, config.NUM_EPOCHS)
     epoch_data_average, loss_data_average = average_epoch_and_loss_data(epoch_data, loss_data)
-    # plot the data for visualization
+
+    print("Plotting data...")
     plot_data(epoch_data_average, loss_data_average)
-    # evaluate the model on images the model hasn't seen using the test dataset
+    
+    print("Testing model...")
     test_model(neural_network)
-    # save the currently trained model's weights
-    # note that this will overwrite any existing file saved under the same path
-    save_model(neural_network, f"{MODEL_WEIGHTS_PATH}/gradient_descent.pt")
+    
+    print("Saving model...")
+    data.save_model(neural_network, data.GRADIENT_DESCENT_MODEL_PATH)
+
+def run_load_mode():
+    print("Loading model...")
+    neural_network = data.load_model(
+        data.GRADIENT_DESCENT_MODEL_PATH,
+        NeuralNetwork()
+    )
+
+    print("Testing model...")
+    test_model(neural_network)
 
 def main():
-    # give the user a choice if he wants to train a new model or test / showcase an existing one
-    mode = input('Train and test model from scratch: 0\nLoad and test existing model: 1\nWhich mode would you like to do: ')
-    if mode == '0':
-        train_model_from_scratch()
-    elif mode == '1':
-        # load an existing model's weights'
-        neural_network = load_model(f"{MODEL_WEIGHTS_PATH}/gradient_descent.pt", NeuralNetwork())
-        # test the existing model
-        test_model(neural_network)
-    else:
-        # have a fail safe for when the user types something else
-        print("Input wasn't accepted. Please try again.")
-        main()
+    train_dataloader = DataLoader(data.TRAIN_DATASET, batch_size=config.BATCH_SIZE)
 
-# number of images per batch
-batch_size = 5
-# load the dataset into the dataloader with a batch size of 5
-# the dataloader will give 1200 batches of 5 images every time (6000 images / 5 images per batch)
-# this is important for training, as you can easily iterate through the dataloader
-train_dataloader = DataLoader(TRAIN_DATASET, batch_size=batch_size)
-# initialize the neural network
-neural_network = NeuralNetwork()
-# set the number of epochs that the model will be trained for
-num_epochs = 20
+    while True:
+        mode = input(
+            'Train and test model with gradient descent: 0\n'
+            'Load and test existing model: 1\n'
+            'Which mode would you like to do: '
+        )
+
+        match mode:
+            case '0':
+                run_training_mode(train_dataloader)
+            case '1':
+                run_load_mode()
+            case _:
+                print("Input wasn't accepted. Please try again.")
 
 if __name__ == '__main__':
     main()
